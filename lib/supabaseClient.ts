ac68ea3213acd748fb942d28dba35e4c74537c6e
Samuel Laniyan <samueltitan@Samuels-MacBook-Pro.local>
2025-12-17 05:44:35 +0000

    fix(env): expose Supabase client vars for TestFlight

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,8 +2,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL_PUBLIC!;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY_PUBLIC!;
 
 // ✅ True singleton Supabase client (created once at module load)
 export const supabase: SupabaseClient = createClient(
